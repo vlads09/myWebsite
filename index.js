500c82da1de89a379342334d939cecf99c9a6393
George-Vlad SIMION <george_vlad.simion@stud.acs.upb.ro>
2025-10-07 20:25:44 +0300

    Demo

---
+++ index.js
@@ -0,0 +1,8 @@
+// press on a button and redirect on another page
+document.getElementById("myButtonGit").addEventListener("click", function() {
+    window.location.href = "https://github.com/vlads09";
+});
+
+document.getElementById("myButtonLinkedIn").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/george-vlad-simion";
+});
